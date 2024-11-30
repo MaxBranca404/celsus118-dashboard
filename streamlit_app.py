38d2e719b0f974ee27e995c2dccc403c642cc34a
MaxBranca404 <130474803+MaxBranca404@users.noreply.github.com>
2024-11-30 13:16:37 +0000

    logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import math
 from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(page_title="Interfaccia Supporto Operatori 118", layout="wide", page_icon="resources/logo.png")
+st.set_page_config(page_title="CELSUS 118", layout="wide", page_icon="resources/logo.png")
 
 # Header
 st.markdown("""
-        <div style='display: flex; align-items: center;'>
-            <img src='/workspaces/celsus118-dashboard/resources/logo.png' width='50' style='margin-right: 15px;'>
-            <h1 style='text-align: center; margin: 0;'>Interfaccia Supporto Operatori 118</h1>
+        <div  style='display: flex; align-items: center;'>
+            <img src='https://github.com/MaxBranca404/celsus118-dashboard/blob/main/resources/logo.png?raw=true' width='50' style='margin-right: 15px;'>
+            <h1 style='text-align: center; margin: 0;'>CELSUS 118</h1>
         </div>
     """, unsafe_allow_html=True)
 
@@ -29,98 +29,7 @@ with st.sidebar:
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# ........
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
 
 st.header('GDP over time', divider='gray')
 
